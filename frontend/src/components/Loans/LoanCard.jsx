73ee00a235cfad8a218dacf093392d7ad807f79f
Avin Joy Dcosta <avinjoy51@gmail.com>
2025-04-19 14:11:23 +0530

    Modify fetching of Data

--- frontend/src/components/Loans/LoanCard.jsx
+++ frontend/src/components/Loans/LoanCard.jsx
@@ -1,31 +1,33 @@
 import React from "react";
 
-function LoanCard() {
-  return (
+function LoanCard({ loan }) {
+  return loan ? (
     <div className=" bg-neutral-950 text-white rounded-2xl p-4">
       <div className="my-2 grid grid-cols-4">
-        <h1 className=" col-span-2">Roshan Anand</h1>
+        <h1 className=" col-span-2">{loan.name}</h1>
         <h2 className=" text-gray-400">
           Installment: <span className=" text-white">Monthly</span>
         </h2>
-        <span className="ml-9 pl-2 size-5.5 bg-blue-500 rounded-full text-white">{">"}</span>
+        <span className="ml-9 pl-2 size-5.5 bg-blue-500 rounded-full text-white">
+          {">"}
+        </span>
       </div>
       <div className=" grid grid-cols-3">
         <div>
           <h4 className=" text-gray-400 ">Loan amount</h4>
-          <h4>₹ 25000</h4>
+          <h4>₹ {loan.loanAmount}</h4>
         </div>
         <div>
           <h4 className=" text-gray-400 ">Tenure</h4>
-          <h4>{"9 Month(s)"}</h4>
+          <h4>{`${loan.tenure} Month(s)`}</h4>
         </div>
         <div>
           <h4 className=" text-gray-400 ">Interest rate</h4>
-          <h4>40.5%</h4>
+          <h4>{loan.interestRate}%</h4>
         </div>
       </div>
     </div>
-  );
+  ) : null;
 }
 
 export default LoanCard;
--- frontend/src/components/Loans/Loans.jsx
+++ frontend/src/components/Loans/Loans.jsx
@@ -1,19 +1,66 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import LoanCard from "./LoanCard";
+import { useSelector } from "react-redux";
 
 function Loans() {
+  const status = useSelector((state) => state.auth.status);
+  const [data, setData] = useState([
+    {
+      id: 1,
+      name: "Roshan",
+      loanAmount: 25000,
+      tenure: 9,
+      interestRate: 40.5,
+      status: "Ongoing",
+    },
+    {
+      id: 2,
+      name: "Roshan",
+      loanAmount: 25000,
+      tenure: 9,
+      interestRate: 40.5,
+      status: "Ongoing",
+    },
+  ]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const res = await fetch("http://localhost:5000/user/loans/requests");
+        if (res.status == 200) {
+          setData(res.data);
+        }
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
+    fetchData();
+  }, [status]);
+
   return (
-    <div className=" p-10 bg-neutral-900">
-      <div className="my-2.5 p-4 rounded-xl bg-black ">
-        <h1 className=" text-4xl my-10 ml-10 text-white font-montserrat font-extrabold
-        ">Live Loans</h1>
-        <div className="grid grid-cols-1 place-items-center gap-y-3 w-11/12">
-          <LoanCard />
-          <LoanCard />
-          <LoanCard />
-          <LoanCard />
-          <LoanCard />
-        </div>
+    <div className=" p-10 h-screen bg-neutral-900">
+      <div className="my-5 p-4 rounded-xl bg-black ">
+        <h1
+          className=" text-4xl my-10 ml-10 text-white font-montserrat font-extrabold
+        "
+        >
+          Live Loans
+        </h1>
+        {data.length != 0 ? (
+          <div className=" grid grid-cols-1 gap-y-5 ">
+            {data.map((item) => (
+              <div key={item.id} className=" grid grid-cols-1 gap-y-5">
+                <LoanCard loan={item} />
+              </div>
+            ))}
+          </div>
+        ) : (
+          <div className="flex justify-center items-center h-56">
+            <AiOutlineInbox className="text-4xl text-gray-500" />
+            <h1 className="text-2xl text-gray-500">No Requested Loans</h1>
+          </div>
+        )}
       </div>
     </div>
   );
